386a824049813b982c5079133020b5fb0c23285f
Hugo <hugo.rock20@hotmail.com>
2018-05-20 23:09:29 -0500

    config datebase

--- README.md
+++
@@ -1,2 +0,0 @@
-# Course_of_PHP7_POO_MySQL_JQuery_Ajax
-PHP7, POO, MySQL, JQuery y Ajax
---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+
+//ip de la pc del servidor de la base de datos
+define("DB_HOST", "localhost");
+
+//Nombre de la base de datos
+define("DB_NAME", "dbsistena");
+
+//Usuario de la base de datos
+define("DB_USERNAME","root");
+
+//Contraseña del usuario de la ase de datos
+define("DB_PASSWORD","");
+
+//Definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","ITVentas");
+
+?>
