f2e0369a3d9d3212acc83ddc11b937c8bfcb4d18
Hugo <hugo.rock20@hotmail.com>
2018-05-22 23:24:29 -0500

    create model Categoria.php

---
+++ models/Categoria.php
@@ -0,0 +1,51 @@
+<?php
+
+//Incluímos inicialmente la conexión a la base de datos
+require '../config/Conexion.php';
+
+class Categoria
+{
+    //Implementamoa nuestro constructor
+    public function __construct(){
+
+    }
+
+    //Implementamos un método para insertar registros
+    public function insertar($nombre, $descripcion){
+        $sql = "INSERT INTO categoria (nombre,descripcion,condicion) VALUES ('$nombre','$descripcion','1')";
+        return ejecutarConsulta($sql);
+    }
+
+    //Implementamos un método para editar registros
+    public function editar($idcategoria, $nombre, $descripcion){
+        $sql = "UPDATE categoria SET nombre='$nombre', descripcion='$descripcion' WHERE idcategoria='$idcategoria'";
+        return ejecutarConsulta($sql);
+    }
+
+    //Implementamos un método para desactivar categorías
+    public function desactivar($idcategoria){
+        $sql = "UPDATE categoria SET condicion='0' WHERE idcategoria='$idcategoria'";
+        return ejecutarConsulta($sql);
+    }
+
+    //Implementamos un método para activar categorías
+    public function activar($idcategoria){
+        $sql = "UPDATE categoria SET condicion='1' WHERE idcategoria='$idcategoria'";
+        return ejecutarConsulta($sql);
+    }
+
+    //Implementamos un método para listar un registro
+    public function mostrar($idcategoria){
+        $sql = "SELECT * FROM categoria WHERE idcategoria='$idcategoria'";
+        return ejecutarConsultaSimpleFila($sql);
+    }
+
+    //Implementar para listar todos los registros
+    public function listar(){
+        $sql = "SELECT * FROM categoria";
+        return ejecutarConsulta($sql);
+    }
+}
+
+
+?>
