41533b06dd6746c1ef03bc3762bddf7c78468127
Hugo <hugo.rock20@hotmail.com>
2018-05-23 22:48:52 -0500

    category.php on ajax

---
+++ app/ajax/categoria.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once '../models/Categoria.php';
+
+$categoria = new Categoria();
+
+$idcategoria = isset($_POST["idcategoria"]) ? limpiarCadena($_POST["idcategoria"]) : "";
+$nombre = isset($_POST["nombre"]) ? limpiarCadena($_POST["nombre"]) : "";
+$descripcion = isset($_POST["descripcion"]) ? limpiarCadena($_POST["descripcion"]) : "";
+
+switch ($_GET["op"]) {
+    case 'guardaryeditar':
+        if (empty($idcategoria)) {
+            $rspta = $categoria->insertar($idcategoria, $nombre, $descripcion);
+            echo $rspta = "Categoría registrada" : "Categoría no se pudo registrar";
+        }else{
+            $rspta = $categoria->editar($nombre, $descripcion);
+            echo $rspta = "Categoría actualizada" : "Categoría no se pudo actualizar";
+        }
+        break;
+    case 'desactivar'
+        $rspta = $categoria->desactivar($idcategoria);
+        echo $rspta ? "Categoría desactivada" : "Categoría no se pudo desactivar";
+        break;
+    case 'activar'
+        $rspta = $categoria->activar($idcategoria);
+        echo $rspta ? "Categoría activada" : "Categoría no se pudo activar";
+        break;
+    case 'mostrar'
+        $rspta = $categoria->mostrar($idcategoria);
+        echo json_encode($rspta);
+        break;
+    case 'listrar'
+        $rspta = $categoria->listar();
+        $data = Array();
+
+        while ($reg = $rspta->fetch_object()) {
+            $data[] = array(
+                "0"=>$reg->idcategoria,
+                "1"=>$reg->nombre,
+                "2"=>$reg->descripcion,
+                "3"=>$reg->condicion
+            );
+        }
+
+        $results = array(
+            "sEcho"=>1, //información para el datatables
+            "iTotalRecors"=>count($data), //enviamos el total de registros al datatable
+            "iTotalDisplayRecords"=>count($data), //enviamos el total de registro a visualizar
+            "aData"=>$data
+        );
+
+        echo json_encode($results);
+
+        break;
+    default:
+        # code...
+        break;
+}
+
+?>
