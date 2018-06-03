0c77a6d07a395b8b663484196b61de500b19473b
Hugo <hugo.rock20@hotmail.com>
2018-06-03 13:06:56 -0500

    entry purchase form HTML for save articles

--- app/views/ingreso.php
+++ app/views/ingreso.php
@@ -59,19 +59,72 @@ if ($_SESSION["compras"] == 1) {
 
                     <div class="panel-body" id="formularioRegistros">
                         <form name="formulario" id="formulario" method="POST">
-                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label>Nombre:</label>
-                                <input type="hidden" name="idcategoria" id="idcategoria">
-                                <input type="text" class="form-control" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" autocomplete="off" required>
+                            <div class="form-group col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                                <label>Proveedor(*):</label>
+                                <input type="hidden" name="idingreso" id="idingreso">
+                                <select name="idproveedor" id="idproveedor" class="form-control selectpicker" data-live-search="true" required></select>
                             </div>
+                            <div class="form-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                <label>Fecha(*):</label>
+                                <input type="date" class="form-control" name="fecha_hora" id="fecha_hora" autocomplete="off" required>
+                            </div>
+
                             <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label>Descripción:</label>
-                                <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="256" placeholder="Descripción" autocomplete="off">
+                                <label>Tipo Comprobante(*):</label>
+                                <select name="tipo_comprobante" id="tipo_comprobante" class="form-control selectpicker" required>
+                                    <option value="Boleta">Boleta</option>
+                                    <option value="Factura">Factura</option>
+                                    <option value="Ticket">Ticket</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-lg-2 col-md-2 col-sm-6 col-xs-12">
+                                <label>Serie(*):</label>
+                                <input type="date" class="form-control" name="serie_comprobante" id="serie_comprobante" autocomplete="off" required maxlength="7" placeholder="Serie">
+                            </div>
+                            <div class="form-group col-lg-2 col-md-2 col-sm-6 col-xs-12">
+                                <label>Número(*):</label>
+                                <input type="date" class="form-control" name="num_comprobante" id="num_comprobante" autocomplete="off" required maxlength="10" placeholder="Número">
+                            </div>
+                            <div class="form-group col-lg-2 col-md-2 col-sm-6 col-xs-12">
+                                <label>Impuesto(*):</label>
+                                <input type="date" class="form-control" name="impuesto" id="impuesto" autocomplete="off" required maxlength="10" placeholder="Impuesto">
+                            </div>
+
+                            <div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                <a data-toggle="modal" href="#myModal">
+                                    <button type="button" id="btnAgregaArt" class="btn btn-primary">
+                                        <span class="fa fa-plus"></span> Agregar Artículos
+                                    </button>
+                                </a>
                             </div>
                             <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                                    <thead style="background-color: #A9D0F5">
+                                        <th>Opciones</th>
+                                        <th>Artículos</th>
+                                        <th>Cantidad</th>
+                                        <th>Precio Compra</th>
+                                        <th>Precio Venta</th>
+                                        <th>Subtotal</th>
+                                    </thead>
+                                    <tfoot>
+                                        <th>Total</th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th><h4 id="total">S/. 0.00</h4><input type="hidden" name="total_compra" id="total_compra"></th>
+                                    </tfoot>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12" id="guardar">
                                 <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
 
-                                <button class="btn btn-danger" onclick="cancelarForm()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                                <button class="btn btn-danger" onclick="cancelarForm()" type="button" id="btnCancelar"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                             </div>
                         </form>
                     </div>
